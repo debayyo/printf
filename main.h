423a959a791e7c378edf16894159d821783eed46
debayyo <adegbitequadriadebayo@gmail.com>
2022-04-19 18:24:08 -0700

    print_address.c

--- main.h
+++ main.h
@@ -7,27 +7,31 @@
 /**
  * struct flags - struct containing flags to "turn on"
  * when a flag specifier is passed to _printf()
+ *
  * @plus: flag for the '+' character
  * @space: flag for the ' ' character
  * @hash: flag for the '#' character
  */
+
 typedef struct flags
 {
-    int plus;
-    int space;
-    int hash;
+	int plus;
+	int space;
+	int hash;
 } flags_t;
 
 /**
  * struct printHandler - struct to choose the right function depending
  * on the format specifier passed to _printf()
+ *
  * @c: format specifier
  * @f: pointer to the correct printing function
  */
+
 typedef struct printHandler
 {
-    char c;
-    int (*f)(va_list ap, flags_t *f);
+	char c;
+	int (*f)(va_list ap, flags_t *f);
 } ph;
 
 /* print_nums */
